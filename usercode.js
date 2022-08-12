632aef0f85638498f308be8d0189fb977c1fe89d
Andrey2108Tymko <48129757+Andrey2108Tymko@users.noreply.github.com>
2022-08-12 17:22:52 +0300

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -3,6 +3,6 @@ const HmacMD5 = require("crypto-js/hmac-md5");
 
 module.exports = (data) => {
     var secret = 'flk3409refn54t54t*FNJRET';
-    var sign = HmacMD5(JSON.stringify('test_merch_n1;www.market.ua;DH1660313179;1415379863;1547.36;UAH;Процессор Intel Core i5-4670 3.4GHz;Память Kingston DDR3-1600 4096MB PC3-12800;1;1;1000;547.36'), secret).toString();
-    return sign;
+    data.sign = HmacMD5(JSON.stringify('test_merch_n1;www.market.ua;DH1660313179;1415379863;1547.36;UAH;Процессор Intel Core i5-4670 3.4GHz;Память Kingston DDR3-1600 4096MB PC3-12800;1;1;1000;547.36'), secret).toString();
+    return data;
 };
