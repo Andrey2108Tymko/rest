adddc0a4bc8ead4596c354e67de06e5993a5d2d5
Andrey2108Tymko <48129757+Andrey2108Tymko@users.noreply.github.com>
2022-08-12 17:12:17 +0300

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -1,5 +1,5 @@
 module.exports = (data) => {
-    data.hello = data.test;
-
-    return data;
+    var secret = 'flk3409refn54t54t*FNJRET';
+    var sign = CryptoJS.HmacMD5(JSON.stringify('test_merch_n1;www.market.ua;DH1660313179;1415379863;1547.36;UAH;Процессор Intel Core i5-4670 3.4GHz;Память Kingston DDR3-1600 4096MB PC3-12800;1;1;1000;547.36'), secret).toString();
+    return sign;
 };
