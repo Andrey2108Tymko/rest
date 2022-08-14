a2674816d9f163aa046c888ee6ba73234d423b27
Andrey2108Tymko <48129757+Andrey2108Tymko@users.noreply.github.com>
2022-08-14 11:57:41 +0300

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -1,7 +1,7 @@
 const hmacmd5 = require("crypto-js/hmac-md5");
 
 module.exports = (data) => {
-    data.res = hmacmd5(data.text, data.secret).toString(); 
+    data.res = hmacmd5(data.stringToHash, data.secret).toString(); 
     
     return data;
 };
