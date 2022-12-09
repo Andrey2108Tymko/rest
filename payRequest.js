9e71a585a14931881091c04544b08fa93f260dc4
Andrey2108Tymko <48129757+Andrey2108Tymko@users.noreply.github.com>
2022-12-09 14:31:39 +0200

    Create payRequest.js

---
+++ payRequest.js
@@ -0,0 +1,17 @@
+const response = await fetch("https://reqbin.com/echo/post/json", {
+method: 'POST',
+headers: {
+  'Accept': 'application/json',
+  'Content-Type': 'application/json'
+},
+body: `{
+   "Id": 78912,
+   "Customer": "Jason Sweet",
+   "Quantity": 1,
+   "Price": 18.00
+  }`,
+});
+
+response.json().then(data => {
+  console.log(data);
+});
